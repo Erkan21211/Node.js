9969c266ad8034c2989e54253401f2a2bc9ae6af
Erkan2 <E.aslantas@hotmail.com>
2022-11-07 01:22:35 +0100

    section update 1

--- Section-1/index.js
+++ Section-1/index.js
@@ -6,21 +6,49 @@ const http = require('http');
 const port = '8000';
 const localhost = '127.0.0.1';
 
+const replace = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.foodName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRIJS%}/g, product.prijs);
+    output = output.replace(/{%FROM%}/g, product.From);
+    output = output.replace(/{%NUTRIENTS%}/g, product.protein);
+    output = output.replace(/{%HOEVEEL%}/g, product.hoeveel); 
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(product.From != "Turkiye") output = output.replace(/{%NOT_KEBAB%}/g, 'not-kebab');
+    return output;
+}
+
+const overview_page = fs.readFileSync(`${__dirname}/page/overview.html`, 'utf-8');
+const card_placeholder = fs.readFileSync(`${__dirname}/page/card.html`, 'utf-8');
+const product_page = fs.readFileSync(`${__dirname}/page/product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
-const READdata = JSON.parse(data);
+const dataObject = JSON.parse(data);
 
 // SERVER
 const server = http.createServer((req, res) => {
-
     // url routing 
     const PathName = req.url;
+
+    // overview pagina
     if(PathName === '/' || PathName === '/overview') {
-        res.end('<h1>overview<h1>');
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        	
+        // loop
+        const cards_html = dataObject.map(el => replace(card_placeholder, el)).join('');
+        const output = overview_page.replace('{%PRODUCT_CARDS%}', cards_html);
+
+        res.end(output);
+    //  api pagina
     } else if (PathName === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json'});
         res.end(data);
+    // product pagina
     } else if(PathName === '/product') {
         res.end('<h1>Products<h1>');
+    // error pagina
     } else {
         res.writeHead(404, { 'Content-type': 'text/html' });
         res.end('<h1>Page not found<h1>');
